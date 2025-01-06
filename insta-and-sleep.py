d4e4617e5656f95168cf2dc9c13928dedeb01d51
Kamil <example@example.com>
2025-01-06 21:41:57 +0100

    fix

--- insta-and-sleep.py
+++ insta-and-sleep.py
@@ -281,7 +281,7 @@ def get_media_likers(login_credentials):
     media_pk = cl.media_pk_from_url(media_link)
     potential_followers = cl.media_likers(media_pk)
 
-    with open('tofollow.txt.'+username_without_special_characters, 'w') as tofollow:
+    with open('tofollow.txt.'+username_without_special_characters, 'r+') as tofollow:
         for potential_follower in potential_followers:
             link_to_save = 'https://www.instagram.com/' + potential_follower.username + '/\n'
             if link_to_save in tofollow.read():
